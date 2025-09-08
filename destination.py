98266d3f364487caf671e05bb1cb6058ef937043
baal <alburquenque.letelier@gmail.com>
2025-09-07 21:40:24 -0300

    Se envia trama en bytes adoc formato micro controlador
    Falta pulir en detalle la estructura de la trama. En concreto, luego del tipo de trama falta determinar que campos van antes del payload.

--- destination.py
+++ destination.py
@@ -10,25 +10,65 @@ DESTINATION_INTERFACE= os.getenv("DESTINATION_INTERFACE")
 
 iface = DESTINATION_INTERFACE
 
+appendix_dict = {
+    b"00": "X_00_CPU",
+    b"02": "X_02_TestTrigger",
+    b"03": "X_03_RO_Single",
+    b"04": "X_04_RO_ON",
+    b"05": "X_05_RO_OFF",
+    b"08": "X_08_DIAG_",
+    b"09": "X_09_DIAG_DIS",
+    b"F9": "X_F9_TTrig_Global",
+    b"FA": "X_FA_TTrig_Local",
+    b"FB": "X_FB_TTrig_Auto_EN",
+    b"FC": "X_FC_TTrig_Auto_DIS",
+    b"FF": "X_FF_Reset",
+    b"20": "X_20_PwrDwnb_TOP_ON",
+    b"21": "X_21_PwrDwnb_TOP_OFF",
+    b"22": "X_22_PwrDwnb_BOT_ON",
+    b"23": "X_23_PwrDwnb_BOT_OFF",
+    b"24": "X_24_PwrEN_2V4A_ON",
+    b"25": "X_25_PwrEN_2V4A_OFF",
+    b"26": "X_26_PwrEN_2V4D_ON",
+    b"27": "X_27_PwrEN_2V4D_OFF",
+    b"28": "X_28_PwrEN_3V1_ON",
+    b"29": "X_29_PwrEN_3V1_OFF",
+    b"2A": "X_2A_PwrEN_1V8A_ON",
+    b"2B": "X_2B_PwrEN_1V8A_OFF",
+    b"E0": "X_E0_FanSpeed0_Low",
+    b"E1": "X_E1_FanSpeed0_High",
+    b"E2": "X_E2_FanSpeed1_Low",
+    b"E3": "X_E3_FanSpeed1_High",
+}
+
 sock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
 sock.bind((iface, 0))
-bufer_size = 65535
+bufer_size = 65535 # n° arbitrario en bytes
 
 while True:
-    raw_data, addr = sock.recvfrom(bufer_size) #usar una var+ verbose
+    raw_data, addr = sock.recvfrom(bufer_size) 
 
     dst_mac = raw_data[0:6].hex(":")
     src_mac = raw_data[6:12].hex(":")
     eth_type = struct.unpack("!H", raw_data[12:14])[0]
-    payload = raw_data[14:]
+    payload = raw_data[14:22]
 
-    if eth_type == 0x9000:  # nuestro tipo "custom"
+    if eth_type == 0x0007:  # nuestro tipo "custom"
         print(f"\n=== Frame received on {iface} ===")
         print("From:", src_mac, "To:", dst_mac)
         print("Raw payload:", payload)
+        print("Payload Parse")
+        parse_payload = {
+            "length": payload[0:2],
+            # "unknowBytes": payload[2:6], #falta averiguar que es
+            # "counter_frame": payload[6:10],
+            # "position_ID": payload[10:11], # falta averiguar que es
+            # "counter": payload[11:12], # falta averiguar que es 
+            "appendix_bytes": payload[6:8],
+            "appendix_name": appendix_dict[payload[6:8]] 
+            # "appendix_name": appendix_dict[payload[12:13]] 
+        }
+        print(parse_payload)
 
-        try:
-            decoded = json.loads(payload.decode("utf-8"))
-            print("Decoded payload:", decoded)
-        except Exception as e:
-            print("Could not decode JSON:", e)
+    else:
+        print("There isn't an etherType frame")
--- source.py
+++ source.py
@@ -15,25 +15,45 @@ DESTINATION_MAC = os.getenv("DESTINATION_MAC")
 def mac_to_bytes(mac):
     return bytes.fromhex(mac.replace(":", ""))
 
-# Mockup
-dataframe = {
-    "metadata": {
-        "temperature": 36.7,
-        "status": "OK",
-        "datetime": datetime.now().strftime("%d:%m:%Y %H:%M:%S")
-    },
-    "data": {
-        "luminosity": 420,
-        "datetime": datetime.now().strftime("%d:%m:%Y %H:%M:%S")
-    }
+## Falta averiguar que es 00:07:00:00:00:00:02:03 ## 
+## 00:07 ¿es el protocolo etherType o custom?
+payload_base = bytes.fromhex("00:07:00:00:00:00:02:03".replace(":", ""))
+appendix_dict = {
+    "X_00_CPU": b"00",
+    "X_02_TestTrigger": b"02",
+    "X_03_RO_Single": b"03",
+    "X_04_RO_ON": b"04",
+    "X_05_RO_OFF": b"05",
+    "X_08_DIAG_": b"08",
+    "X_09_DIAG_DIS": b"09",
+    "X_F9_TTrig_Global": b"F9",
+    "X_FA_TTrig_Local": b"FA",
+    "X_FB_TTrig_Auto_EN": b"FB",
+    "X_FC_TTrig_Auto_DIS": b"FC",
+    "X_FF_Reset": b"FF",
+    "X_20_PwrDwnb_TOP_ON": b"20",
+    "X_21_PwrDwnb_TOP_OFF": b"21",
+    "X_22_PwrDwnb_BOT_ON": b"22",
+    "X_23_PwrDwnb_BOT_OFF": b"23",
+    "X_24_PwrEN_2V4A_ON": b"24",
+    "X_25_PwrEN_2V4A_OFF": b"25",
+    "X_26_PwrEN_2V4D_ON": b"26",
+    "X_27_PwrEN_2V4D_OFF": b"27",
+    "X_28_PwrEN_3V1_ON": b"28",
+    "X_29_PwrEN_3V1_OFF": b"29",
+    "X_2A_PwrEN_1V8A_ON": b"2A",
+    "X_2B_PwrEN_1V8A_OFF": b"2B",
+    "X_E0_FanSpeed0_Low": b"E0",
+    "X_E1_FanSpeed0_High": b"E1",
+    "X_E2_FanSpeed1_Low": b"E2",
+    "X_E3_FanSpeed1_High": b"E3",
 }
 
-payload = json.dumps(dataframe).encode("utf-8")
-
 # Construcción de cabecera Ethernet: [MAC_dst][MAC_src][EtherType]
-eth_header = mac_to_bytes(DESTINATION_MAC) + mac_to_bytes(SOURCE_MAC) + struct.pack("!H", 0x9000)
+eth_header = mac_to_bytes(DESTINATION_MAC) + mac_to_bytes(SOURCE_MAC)
 
 # Frame final
+payload = payload_base + appendix_dict["X_02_TestTrigger"] # Cambiarlo según corresponda
 frame = eth_header + payload
 
 print("=== Data frame builded ===")
